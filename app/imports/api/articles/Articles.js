cd733fe70ba9f35279e758712874a57fdcdb8ec2
jsapolu99 <109643051+jsapolu99@users.noreply.github.com>
2023-11-04 01:54:30 -1000

    Changed ArticlesCollection:
    The title of each article is now saved to a subject property.
    
    The body of each article is now saved to a content property.
    
    NOTE: Still must change path to absolute path to work.
    
    Also all html tags are stored still although in a String format.

--- app/imports/api/articles/Articles.js
+++ app/imports/api/articles/Articles.js
@@ -1,11 +1,30 @@
 import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
 
 /**
- * The StuffsCollection. It encapsulates state and variable values for article.
+ * The ArticlesCollection. It encapsulates state and variable values for stuff.
  */
+class ArticlesCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ArticlesCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      subject: String,
+      content: String,
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
 
 /**
- * The singleton instance of the ArticleCollection.
- * @type {ArticleCollection}
+ * The singleton instance of the StuffsCollection.
+ * @type {ArticlesCollection}
  */
-export const Articles = new Mongo.Collection('Articles');
+export const Articles = new ArticlesCollection();
