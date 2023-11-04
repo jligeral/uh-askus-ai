fa7f504a6a2026888ffbabfe3bc11bd29bab2a57
James Ligeralde <jligeral@hawaii.edu>
2023-11-03 18:29:15 -1000

    Set up Articles collection and loaded all html articles in MongoDB
    Must set user's absolute path for database to work

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,40 +1,27 @@
 import { Meteor } from 'meteor/meteor';
-// import fs from 'fs';
-// import path from 'path';
-import { Stuffs } from '../../api/stuff/Stuff.js';
-// import { Articles } from '../../api/articles/Articles';
+import fs from 'fs';
+import path from 'path';
+import { Articles } from '../../api/articles/Articles';
 
 /* eslint-disable no-console */
 
-// Initialize the database with a default data document.
-const addData = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
+// Initialize the database with a all articles
+const addArticle = (article) => {
+  Articles.insert(article);
 };
 
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
-  }
-}
-/*
-// Use __dirname to get the directory of the current script
-const currentDirectory = __dirname;
+// Use Absolute Path here for now
+const ArticlesDir = ('/absolute/path/to//uh-askus-ai/app/public/articles');
 
-// Construct the path to the '/imports/public/articles' directory
-
-const ArticlesDir = path.join(currentDirectory, '../../../public/articles');
-
-// Initialize the HtmlFiles collection if empty.
+// Initialize the Articles collection if empty.
 if (Articles.find().count() === 0) {
-  console.log('Creating default data.');
+  console.log('Loading articles');
 
-  // Read HTML files and add them to the HtmlFiles collection
-  fs.readdirSync(ArticlesDir).forEach((file) => {
-    const content = fs.readFileSync(path.join(ArticlesDir, file), 'utf8');
-    addData({ name: file, content });
+  // Read HTML files and add them to the Articles collection
+  fs.readdirSync(ArticlesDir).forEach((article) => {
+    const content = fs.readFileSync(path.join(ArticlesDir, article), 'utf8');
+    addArticle({ fileName: article, content });
+    // console.log(`  Adding: ${article}`);
   });
+console.log('All articles loaded!');  
 }
-*/
