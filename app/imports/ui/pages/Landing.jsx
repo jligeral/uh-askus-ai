9ac59af49f6da3f3554e0fc89e5e2038a3f2a80b
James Ligeralde <jligeral@hawaii.edu>
2023-11-05 07:48:22 -1000

    Fixed errors

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Col, Container, Image, Row } from 'react-bootstrap';
-import { Articles } from '../../api/articles/Articles';
 
 /* A simple static component to render some text for the landing page. */
-const Landing = () => {
+const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
     <Row className="align-middle text-center">
       <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px"/>
+        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
       </Col>
 
       <Col xs={8} className="d-flex flex-column justify-content-center">
@@ -17,5 +16,5 @@ const Landing = () => {
 
     </Row>
   </Container>
-};
+);
 export default Landing;
