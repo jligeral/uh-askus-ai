361616c5a8ab38a95ff0aa9980c30067348bfcb1
frances-uy <francesmichuy@gmail.com>
2023-11-05 22:42:10 -1000

    Added Sources Col and Scrolling Feature

--- app/imports/ui/components/Search.jsx
+++ app/imports/ui/components/Search.jsx
@@ -1,4 +1,4 @@
-import { Col, Container, Form, InputGroup, Row } from 'react-bootstrap';
+import { Col, Container, Form, InputGroup, Row, Card } from 'react-bootstrap';
 import { Search } from 'react-bootstrap-icons';
 import React from 'react';
 import ChatItem from './ChatItem';
@@ -13,23 +13,57 @@ const chatMessages = [
   { role: 'assistant', content: 'Thank you for the information. Let me check the pricing for XYZ in New York.' },
 ];
 
+const sources = [
+  { id: 1, title: 'Wifi UH Login', description: 'Connecting in the Classroom', href: '#' },
+  { id: 2, title: 'DUO Required for Everyone', description: 'Downloading the DUO App', href: '#' },
+  { id: 3, title: 'Financial Aid', description: 'Federal Work-Study', href: '#' },
+  { id: 4, title: 'STAR', description: 'Registration Times', href: '#' },
+  { id: 5, title: 'STAR', description: 'Parking Passes', href: '#' },
+  { id: 6, title: 'Wifi UH Login', description: 'Not Connecting', href: '#' },
+  { id: 7, title: 'Zoom', description: 'Zoom Room Time Limit', href: '#' },
+  { id: 8, title: 'Google Maps', description: 'Administrator Access Needed', href: '#' },
+  { id: 9, title: 'Topic: Laulima', description: 'Multi-Factor Authentication (MFA)', href: '#' },
+  { id: 10, title: 'Topic: Laulima', description: 'Multi-Factor Authentication (MFA)', href: '#' },
+  { id: 11, title: 'Topic: Laulima', description: 'Multi-Factor Authentication (MFA)', href: '#' },
+  { id: 12, title: 'Topic: Laulima', description: 'Multi-Factor Authentication (MFA)', href: '#' },
+  { id: 13, title: 'Topic: Laulima', description: 'Multi-Factor Authentication (MFA)', href: '#' },
+  { id: 14, title: 'Topic: Laulima', description: 'Multi-Factor Authentication (MFA)', href: '#' },
+  { id: 15, title: 'Topic: Laulima', description: 'Multi-Factor Authentication (MFA)', href: '#' },
+  { id: 16, title: 'Topic: Laulima', description: 'Multi-Factor Authentication (MFA)', href: '#' },
+];
+
 const ITSearch = () => (
   <Container id="landing-page" fluid className="py-3">
     <Row>
-      <Col xs={4}>
+      <i><h1>Chat with a Virtual Assistant</h1></i>
+    </Row>
+    <Row>
+      <Col xs={5}>
         <InputGroup className="mb-3 search-bar-input-group">
           <Form.Control
-            placeholder="Ask a question"
+            placeholder="Ask a question (Ex: How to use DUO?)"
             aria-label="Recipient's username"
             aria-describedby="basic-addon2"
           />
           <InputGroup.Text id="btn-group"><Search /></InputGroup.Text>
         </InputGroup>
       </Col>
-
-      <Col xs={8} className="d-flex flex-column justify-content-start">
+      <Col xs={4} className="d-flex flex-column scrollable-col" style={{ maxHeight: '80vh', overflowY: 'auto' }}>
+        <h3>Chat History</h3>
         {chatMessages.map((chat, index) => (
           <ChatItem content={chat.content} role={chat.role} key={index} />
+        ))}
+      </Col>
+
+      <Col xs={3} className="d-flex flex-column scrollable-col" style={{ maxHeight: '80vh', overflowY: 'auto' }}>
+        <h3>Sources</h3>
+        {sources.map((source, index) => (
+          <Card key={index} className="mb-2">
+            <Card.Header>{source.title}</Card.Header>
+            <Card.Body>
+              <a href={source.href} target="_blank" rel="noopener noreferrer">{source.description}</a>
+            </Card.Body>
+          </Card>
         ))}
       </Col>
 
