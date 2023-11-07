9a5702bbbb0c3632fbf98f16173ab8511edd858f
James Ligeralde <jligeral@hawaii.edu>
2023-11-06 15:24:01 -1000

    Update Mongo.js

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 import { cheerio } from 'meteor/mrt:cheerio';
-import { Meteor } from 'meteor/meteor';
 import { Articles } from '../../api/articles/Articles';
 
 /* eslint-disable no-console */
@@ -10,28 +9,27 @@ import { Articles } from '../../api/articles/Articles';
 const addArticle = (fileName, content) => {
   Articles.collection.insert({ subject: fileName, content: content });
 };
+// Use Absolute Path here for now
+// Get the current working directory (Meteor project's root directory)
+const projectDir = process.env.PWD;
 
-/* eslint-disable no-console */
+// Define the relative path to the articles directory
+const relativeArticlesPath = 'public/articles';
 
-Meteor.methods({
-  loadArticles() {
-    const currentDir = process.cwd();
-    const articlesDir = path.join(currentDir, 'public/articles');
+// Construct the full path to the articles directory
+const ArticlesDir = path.join(projectDir, relativeArticlesPath);
 
-    if (fs.existsSync(articlesDir) && Articles.collection.find().count() === 0) {
-      console.log('Loading articles');
-      fs.readdirSync(articlesDir).forEach((article) => {
-        const body = fs.readFileSync(path.join(articlesDir, article), 'utf8');
-        const $ = cheerio.load(body);
-        const subject = $('title').toString();
-        const content = $('body').toString();
-        addArticle(subject, content);
-        // console.log(`  Adding: ${subject}`);
-      });
-      console.log('All articles loaded!');
-    }
-  },
-});
-
-// Call the method on the server to load articles
-Meteor.call('loadArticles');
+// Initialize the Articles collection if empty.
+if (Articles.collection.find().count() === 0) {
+  console.log('Loading articles');
+  // Read HTML files and add them to the Articles collection
+  fs.readdirSync(ArticlesDir).forEach((article) => {
+    const body = fs.readFileSync(path.join(ArticlesDir, article), 'utf8');
+    const $ = cheerio.load(body);
+    const subject = $('title').toString();
+    const content = $('body').toString();
+    addArticle(subject, content);
+    // console.log(`  Adding: ${subject}`);
+  });
+  console.log('All articles loaded!');
+}
