26fd49bec18e68605a900ea508094b1d012e23dd
jsapolu99 <109643051+jsapolu99@users.noreply.github.com>
2023-11-11 21:30:57 -1000

    Update Pinecone.js

--- app/imports/startup/server/Pinecone.js
+++ app/imports/startup/server/Pinecone.js
@@ -1,7 +1,6 @@
 import { Pinecone } from '@pinecone-database/pinecone';
 import { Meteor } from 'meteor/meteor';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-import { PineconeStore } from 'langchain/dist/vectorstores/pinecone';
 import { OpenAIEmbeddings } from 'langchain/dist/embeddings/openai';
 import { Articles } from '../../api/articles/Articles';
 // import { getEmbedding } from '../../../server/getEmbedding';
@@ -11,16 +10,16 @@ const pinecone = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY,
   environment: process.env.PINECONE_ENVIRONMENT,
 });
+const embeddings = new OpenAIEmbeddings();
 
 const index = pinecone.index(process.env.PINECONE_INDEX);
 
 const articles = Articles.collection.find().fetch();
 let docs = '';
-for (let i = 0; i < articles.length; i++) {
+for (let i = 0; i < 5; i++) {
   docs += articles[i].content;
   docs = docs.trim();
 }
-const embeddingArray = [];
 
 Meteor.methods({
   addEmbeddingstoDatabse: async function () {
@@ -29,25 +28,24 @@ Meteor.methods({
       chunkOverlap: 200,
     });
 
+    const embeddingArray = [];
     const chunks = await splitter.createDocuments([docs]);
 
-    for (let i = 0; i < chunks.length; i++) {
+    for (let i = 0; i < 5; i++) {
       const chunk = chunks[i];
       // eslint-disable-next-line no-await-in-loop
-      // const embedding = await getEmbedding(chunk.pageContent);
+      const embedding = await embeddings.embedQuery(chunk.pageContent);
       // Assuming you want to do something with the embedding here
       embeddingArray.push({
         id: i.toString(),
+        values: embedding,
         metadata: { content: chunk.pageContent },
       });
-      // console.log('Embedding:', embedding);
-
-      // Add embedding to Pinecone database
-      // eslint-disable-next-line no-await-in-loop
+      console.log(embeddingArray[i]);
     }
-    await PineconeStore.fromDocuments(embeddingArray, new OpenAIEmbeddings(), { pineconeIndex: index, maxConcurrency: 5 });
-    console.log('Embeddings added to database');
-    pinecone.describeIndex('uh-askus-ai');
+    // eslint-disable-next-line no-await-in-loop
+    // Add embedding to Pinecone database
+    await index.upsert(embeddingArray);
   },
 });
 
